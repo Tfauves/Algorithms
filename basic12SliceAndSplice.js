6b20ae4831373e9f066bc6b49fff7042033961eb
Tfauves <68396275+Tfauves@users.noreply.github.com>
2021-06-24 13:50:40 -0400

    Add files via upload
    fcc basic#12

---
+++ basic12SliceAndSplice.js
@@ -0,0 +1,25 @@
+// Slice and Splice
+// You are given two arrays and an index.
+
+// Copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+
+function frankenSplice(arr1, arr2, n) {
+
+    let newArr2 = arr2.slice()
+    // console.log(newArr2)
+    for (let i = 0; i < arr1.length; i++) {
+        // console.log(arr1[i])
+        newArr2.splice(n, 0,  arr1[i])
+        n++
+        // console.log(n)
+    }
+    return newArr2
+}
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
